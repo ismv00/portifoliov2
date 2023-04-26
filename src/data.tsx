8f50126cc7e6450f971cae31993b1f62e3aabd27
ismv00 <ismv00@icloud.com>
2023-04-25 23:49:32 -0400

    fix:ajustes finais02

--- src/data.tsx
+++ src/data.tsx
@@ -1,4 +1,4 @@
-import { FaGithub, FaLinkedin, FaTwitter, FaWhatsapp } from "react-icons/fa";
+import { FaGithub, FaLinkedin, FaWhatsapp } from "react-icons/fa";
 import project1img from "./assets/projects/project1.png";
 import project2img from "./assets/projects/project2.png";
 import project3img from "./assets/projects/project3.png";
