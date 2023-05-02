bbbe88375a4b43b394d1dbf14712ad5ff6d84f4c
ismv00 <ismv00@icloud.com>
2023-05-01 22:43:16 -0400

    fix:about fade

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -5,7 +5,11 @@ export function About() {
   const { text } = aboutData;
   return (
     <div className="w-4/5 m-auto mt-12">
-      <div className="text-base text-center text-slate-600 italic">
+      <div
+        className="text-base text-center text-slate-600 italic"
+        data-aos="fade-up"
+        data-aos-delay="800"
+      >
         <FaQuoteLeft className="text-xs" />
         {text}
         <FaQuoteRight className="text-xs" />
